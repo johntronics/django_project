908132d251494beb9a714553b05c5bed0e877705
johntronics <64572297+johntronics@users.noreply.github.com>
2026-06-30 09:25:01 +0000

    fixed settings file

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@nc^ovm*pz^xi910=e*wvqhe@oazx(wz3xt8p&ib8g-f-$*v8a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
